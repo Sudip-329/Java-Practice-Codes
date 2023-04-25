e9b62abcbdafc9552fcbd95cae28b401a469dfef
Sudip-329 <sudipchakrabarty6@gmail.com>
2023-04-26 00:57:01 +0600

    17.03.23

---
+++ l6q2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class l6q2{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s1,s2;
+        System.out.println("Enter your first name : ");
+        s1 = sc.nextLine();
+        System.out.println("Enter your last name : ");
+        s2 = sc.nextLine();
+        System.out.println(s2+" "+s1);
+    }
+}
